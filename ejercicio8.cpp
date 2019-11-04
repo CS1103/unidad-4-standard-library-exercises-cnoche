e8fea218229b6578b67b8720d101ee14bfab310e
cnoche <claudia.noche@utec.edu.pe>
2019-11-04 14:09:19 -0500

    changes

---
+++ ejercicio8.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<cstdlib>
+#include<algorithm>
+#include<utility>
+using namespace std;
+
+bool cmp(const pair<int, string> one,const pair<int, string> two) {
+    return one.first < two.first;
+}
